707a1c0dd2c6992ea658f8a9e8efa45abffe0ae7
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-08-30 14:16:02 +0700

    benerin function

--- phase_0/week2/kamis/function.js
+++ phase_0/week2/kamis/function.js
@@ -1,34 +1,34 @@
 //FUNCTION
 
 //1
-// function shoutOut(){
-//     console.log('Halo Function')
-// }
+function shoutOut(){
+    console.log('Halo Function')
+}
 
-// console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+console.log(shoutOut()) // Menampilkan "Halo Function!" di console
 
 //2
-// function calculateMultiply(num1, num2) {
-//     return num1 * num2
-// }
-// var num1 = 5;
-// var num2 = 6;
+function calculateMultiply(num1, num2) {
+    return num1 * num2
+}
+var num1 = 5;
+var num2 = 6;
 
-// var hasilPerkalian = calculateMultiply(num1, num2);
-// console.log(hasilPerkalian); // Menampilkan angka 30
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
 
 //3
-// function processSentence(name,age,address,hobby) {
-//     return console.log('Nama saya ' + name + ', umur saya ' + age + ' tahun,' + ' alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + ' !')
+function processSentence(name,age,address,hobby) {
+    return console.log('Nama saya ' + name + ', umur saya ' + age + ' tahun,' + ' alamat saya di ' + address + ', dan saya punya hobby yaitu ' + hobby + ' !')
      
-// }
+}
 
-// var name = "Agus";
-// var age = 30;
-// var address = "Jln. Malioboro, Yogjakarta";
-// var hobby = "gaming";
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
 
-// var fullSentence = processSentence(name,age,address,hobby);
-// console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
 
 
