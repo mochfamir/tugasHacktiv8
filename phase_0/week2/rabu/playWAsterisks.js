2cadd6b1abdb84b765c40286e6685bf6786b297b
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-09-02 21:23:54 +0700

    nambahin for

--- phase_0/week2/rabu/playWAsterisks.js
+++ phase_0/week2/rabu/playWAsterisks.js
@@ -7,12 +7,12 @@ var k = 1;
 var l = 1;
 
 
-// //1 
-for (var i = 1; i <= rows; i++){
+//1 
+for (var i = 1; i <= rows; i++) {
     console.log('*')
 }
 
-// 2
+// 2 ver1
 while (j <= rows) {
     while (k <= rows) {
         star = star + '*'
@@ -21,6 +21,16 @@ while (j <= rows) {
     console.log(star);
     j++;
 }
+
+// 2 ver fix
+for (var m = 1; m <= 5; m++) {
+    for (var n = 1; n <= 5; n++) {
+        star2 = star2 + '*';
+    }
+    console.log(star2)
+    star2 = ''
+}
+
 
 //3
 for (var l = 1; l <= rows; l++) {
