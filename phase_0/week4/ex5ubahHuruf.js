84c38dfd93ddc79492acdfdf05c6ace2d36169b0
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-09-15 16:05:14 +0700

    ubah huruf

---
+++ phase_0/week4/ex5ubahHuruf.js
@@ -0,0 +1,23 @@
+function ubahHuruf(kata) {
+    huruf = 'abcdefghijklmnopqrstuvwxyza'
+    output = ""
+
+    // bandingkan huruf dengan kata
+    for (var i = 0; i < kata.length; i++) {
+        for (var j = 0; j < huruf.length-1; j++) {
+            // jika index kata sama dengan index huruf
+            if (kata[i] == huruf[j]) {
+                // tampilkan index huruf + 1
+                output = output + huruf[j + 1]
+            }
+        }
+    }
+    return output
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
