7861d1733377b1504eb9e31c851aa6bc2930ed25
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-09-16 20:55:44 +0700

    fix

--- phase_0/week4/ex17totalDigit.js
+++ phase_0/week4/ex17totalDigit.js
@@ -1,28 +1,27 @@
-// var hitung = function(val) {
-//     if (val > 0) {
-//         console.log(val);
-//         return hitung(val - 1);
-//     } else {
-//         return val;
-//     }
-// };
-// hitung(10);
-
 function totalDigitRekursif(angka) {
-    angka = angka.toString()
-    if (angka.length <= 1) {
-        return angka
+    //jika angka < 10 kembalikan nilai angka
+    if (angka <= 10) {
+      return angka    
     }
     else {
-        console.log(angka)
-        angka = Number(angka[0]) + totalDigitRekursif(angka.length-1)
-        return angka
+      // ubah dulu ke string
+      var angkaStr = angka.toString()
+      var angka0 = Number(angkaStr[0])      //ambil indeks 0, jadikan number
+      angkaStr = angkaStr.slice(1)          //ambil nilai indeks 1, lalu potong indeks1
+      //console.log(angkaStr)          
+      angkaStr = Number(angkaStr)           //ubah ke number
+      return angka0 + totalDigitRekursif(angkaStr) 
     }
-}
-
-// TEST CASES
-console.log(totalDigitRekursif(512)); // 8
- console.log(totalDigitRekursif(1542)); // 12
- console.log(totalDigitRekursif(5)); // 5
- console.log(totalDigitRekursif(21)); // 3
- console.log(totalDigitRekursif(11111)); // 5
+  
+  }
+  
+  // TEST CASES
+  console.log(totalDigitRekursif(512)); // 8
+  // console.log(totalDigitRekursif(1542)); // 12
+  // console.log(totalDigitRekursif(5)); // 5
+  // console.log(totalDigitRekursif(21)); // 3
+  // console.log(totalDigitRekursif(11111)); // 5
+  
+  
+  
+  
