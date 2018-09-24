466cc3353501683c79785f49fc9ed8866ab4ac9a
Mochamad Faishal Amir <faishalkings@gmail.com>
2018-09-24 14:47:44 +0700

    fix looping yang kelebihan

--- phase_0/week5/ex2naikAngkot.js
+++ phase_0/week5/ex2naikAngkot.js
@@ -1,30 +1,25 @@
 function naikAngkot(arrPenumpang) {
     rute = ['A', 'B', 'C', 'D', 'E', 'F'];
     var output = [];
+    var bayar = 0
     var naik = 0;
     var turun = 0;
     for (var i = 0; i < arrPenumpang.length; i++) {
-        for (var j = 0; j < arrPenumpang[i].length; j++) {
-            obj = {
-                penumpang: arrPenumpang[i][0],
-                naikDari: arrPenumpang[i][1],
-                tujuan: arrPenumpang[i][2],
-                bayar: 0
-            }
-        }
-        output.push(obj)
         for (var j = 0; j < rute.length; j++) {
-            if (rute[j] == arrPenumpang[i][1]) {
+            if (arrPenumpang[i][1] === rute[j]) {
                 naik = j
             }
-            if (rute[j] == arrPenumpang[i][2]) {
+            else if (arrPenumpang[i][2] === rute[j]) {
                 turun = j
-            }   
+            }
         }
-        var hasil = 0;
-        hasil = (turun-naik) * 2000
-        output[i].bayar = hasil
-
+        bayar = (turun-naik)*2000
+        output.push({
+            penumpang: arrPenumpang[i][0],
+            naikDari: arrPenumpang[i][1],
+            tujuan: arrPenumpang[i][2],
+            bayar: bayar
+        })
     }
     return output
 }
